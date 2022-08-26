2bf130570c25862bdfb07c4f152b6fb9034b77d8
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-26 18:45:37 -0500

    Extras

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -7,8 +7,10 @@ function TaskForm({createTask}) {
     const handleSubmit = (e) =>{
         //Esto es como modificar la constante en App.jsx
         e.preventDefault();
-        
         createTask(title, description)
+        //Despues de eviar datos limpiamos formulario
+        setTitle("")
+        setDescription("")
     }
 
     return (
@@ -16,12 +18,21 @@ function TaskForm({createTask}) {
         <form onSubmit={handleSubmit}>
             <input 
                 placeholder='Escribir una tarea' 
-                onChange={ (e) => {setTitle(e.target.value)
-                }}/>
+                onChange=   { (e)=> {
+                                    setTitle(e.target.value)
+                                    }
+                            }
+                value = {title /*Se anade esto para limpiar formulario como definimos en la linea 11*/}
+                autoFocus 
+                />{/*autofocus: al recarar el cursor regresa al input */}
             <textarea 
                 placeholder='Escribir una descripcion' 
-                onChange={ (e) => {setDescription(e.target.value)
-                }}></textarea>
+                onChange=   { (e)=>{setDescription(e.target.value)
+                
+                                    }
+                            }
+                value = {description /*Se anade esto para limpiar formulario como definimos en la linea 11*/}
+                 ></textarea>
                 
             <button>Guardar</button>
         </form>
